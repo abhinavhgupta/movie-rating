5c8a85c146a44a29c11cf13205f448ae7f70fae1
Abhinav Gupta <Abhinav.Gupta@signetjewelers.com>
2019-09-16 16:24:27 +0530

    test cases udpdated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,33 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MovieComponent } from './movie/movie.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Store, StoreModule } from '@ngrx/store';
+import { moviesData } from './data';
+import { movieReducer } from './store/movie.reducer';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
 
 describe('AppComponent', () => {
+  let store: Store<any>;
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let randomButton: DebugElement;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
-      ],
+        AppComponent, MovieComponent
+      ], imports: [ReactiveFormsModule, FormsModule,
+        StoreModule.forRoot({ 'reducers': movieReducer }),],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    component.mov = moviesData;
+    component.movies = moviesData;
+    randomButton = fixture.debugElement.query(By.css('button'));
+
   }));
 
   it('should create the app', () => {
@@ -16,16 +36,30 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'counter'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('counter');
+  it(`should have as title 'Movie List'`, () => {
+    fixture.detectChanges();
+    expect(component.title).toEqual('Movie List');
+  });
+
+  it(`should click randomRating Button`, () => {
+
+    spyOn(component, 'randomRating').and.callThrough();
+
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      expect(component.randomRating).toBeDefined();
+    });
+
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
+    const comp = fixture.componentInstance;
+    comp.movies = moviesData;
+    comp.mov = moviesData;
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to counter!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Movie List!');
   });
 });
--- src/app/movie/movie.component.spec.ts
+++ src/app/movie/movie.component.spec.ts
@@ -1,25 +1,58 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieComponent } from './movie.component';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { movieReducer } from '../store/movie.reducer';
 
 describe('MovieComponent', () => {
   let component: MovieComponent;
   let fixture: ComponentFixture<MovieComponent>;
+  let fb: FormBuilder = new FormBuilder();
+  const moviesData = [
+    {
+      "name": "How to Train Your Dragon",
+      "rating": 10
+    },
+    {
+      "name": "Despicable Me",
+      "rating": 1
+    },
+    {
+      "name": "Tangled",
+      "rating": 2
+    },
+    {
+      "name": "Iron Man 2",
+      "rating": 3
+    }];
 
+  let group: any = {};
+
+  moviesData.forEach(movie => {
+    group[movie.name] = new FormControl(movie.rating);
+  });
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MovieComponent ]
+      declarations: [MovieComponent], imports: [ReactiveFormsModule, FormsModule,
+        StoreModule.forRoot({ 'reducers': movieReducer }),]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MovieComponent);
     component = fixture.componentInstance;
+    component.form = new FormGroup(group);
+    component.movie = moviesData[0];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create a FormGroup comprised of FormControls', () => {
+    expect(component.form instanceof FormGroup).toBe(true);
   });
 });
